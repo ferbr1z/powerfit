b3f6e316bd33d31111036f99e17a8bb8df95b0cf
Marcelo Ferreira <marceloferreira@fiuni.edu.py>
2024-02-22 09:36:57 -0300

    Implementado todo el servicio de cliente

--- src/main/java/com/devs/powerfit/interfaces/IService.java
+++ src/main/java/com/devs/powerfit/interfaces/IService.java
@@ -1,13 +1,14 @@
 package com.devs.powerfit.interfaces;
 
 import com.devs.powerfit.abstracts.AbstractDto;
+import com.devs.powerfit.dtos.PageResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface IService<T extends AbstractDto> {
     public T create(T t);
     public T getById(Long id);
-    public Page<T> getAll(Pageable pag);
+    public PageResponse<T> getAll(int pag);
     public T update(Long id, T t);
     public boolean delete(Long id);
 }
--- src/main/java/com/devs/powerfit/services/ClienteService.java
+++ src/main/java/com/devs/powerfit/services/ClienteService.java
@@ -4,10 +4,13 @@ import com.devs.powerfit.beans.ClienteBean;
 import com.devs.powerfit.daos.ClienteDao;
 import com.devs.powerfit.dtos.ClienteDto;
 import com.devs.powerfit.dtos.PageResponse;
+import com.devs.powerfit.exceptions.NotFoundException;
 import com.devs.powerfit.interfaces.IClienteService;
 import com.devs.powerfit.interfaces.IMapper;
+import com.devs.powerfit.utils.Setting;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -33,30 +36,96 @@ public class ClienteService implements IClienteService {
 
     @Override
     public ClienteDto getById(Long id) {
-        return null;
+        var cliente = clienteDao.findByIdAndActiveTrue(id);
+        if (cliente.isPresent()) {
+            return mapper.toDto(cliente.get(), ClienteDto.class);
+        }
+        throw new NotFoundException("Cliente no encontrado");
+
     }
 
     @Override
-    public Page<ClienteDto> getAll(Pageable pag) {
-        return null;
+    public PageResponse<ClienteDto> getAll(int page) {
+        var pag = PageRequest.of(page - 1, Setting.PAGE_SIZE);
+        var clientes = clienteDao.findAllByActiveTrue(pag);
+
+        if (clientes.isEmpty()) {
+            throw new NotFoundException("No hay clientes en la lista");
+        }
+
+        var clientesDto = clientes.map(cliente -> mapper.toDto(cliente, ClienteDto.class));
+        var pageResponse = new PageResponse<ClienteDto>(clientesDto.getContent(),
+                clientesDto.getTotalPages(),
+                clientesDto.getTotalElements(),
+                clientesDto.getNumber() + 1);
+        return pageResponse;
     }
 
     @Override
     public ClienteDto update(Long id, ClienteDto clienteDto) {
-        return null;
+        var cliente = clienteDao.findByIdAndActiveTrue(id);
+        if (cliente.isPresent()) {
+            var clienteBean = cliente.get();
+
+            if (clienteDto.getNombre() != null) clienteBean.setNombre(clienteDto.getNombre());
+            if (clienteDto.getCedula() != null) clienteBean.setCedula(clienteDto.getCedula());
+            if (clienteDto.getRuc() != null) clienteBean.setRuc(clienteDto.getRuc());
+            if (clienteDto.getTelefono() != null) clienteBean.setTelefono(clienteDto.getTelefono());
+            if (clienteDto.getEmail() != null) clienteBean.setEmail(clienteDto.getEmail());
+            if (clienteDto.getDireccion() != null) clienteBean.setDireccion(clienteDto.getDireccion());
+
+            clienteDao.save(clienteBean);
+
+            return mapper.toDto(clienteBean, ClienteDto.class);
+        }
+        throw new NotFoundException("Cliente no encontrado");
     }
 
     @Override
     public boolean delete(Long id) {
-        return false;
+        var cliente = clienteDao.findByIdAndActiveTrue(id);
+        if (cliente.isPresent()) {
+            var clienteBean = cliente.get();
+            clienteBean.setActive(false);
+            clienteDao.save(clienteBean);
+            return true;
+        }
+        throw new NotFoundException("Cliente no encontrado");
     }
     @Override
     public PageResponse<ClienteDto> searchByNombre(String nombre, int page) {
-        return null;
+        var pag = PageRequest.of(page - 1, Setting.PAGE_SIZE);
+        var clientes = clienteDao.findByNombreAndActiveIsTrue(pag, nombre);
+
+        if (clientes.isEmpty()) {
+            throw new NotFoundException("No hay clientes en la lista");
+        }
+
+        var clientesDto = clientes.map(cliente -> mapper.toDto(cliente, ClienteDto.class));
+        var pageResponse = new PageResponse<ClienteDto>(
+                clientesDto.getContent(),
+                clientesDto.getTotalPages(),
+                clientesDto.getTotalElements(),
+                clientesDto.getNumber() + 1);
+        return pageResponse;
     }
 
     @Override
     public PageResponse<ClienteDto> searchByCi(String ci, int page) {
-        return null;
+        var pag = PageRequest.of(page - 1, Setting.PAGE_SIZE);
+        var clientes = clienteDao.findByCedulaAndActiveIsTrue(pag, ci);
+
+        if(clientes.isEmpty()){
+            throw new NotFoundException("No hay clientes con esa cedula");
+        }
+
+        var clientesDto = clientes.map(cliente -> mapper.toDto(cliente, ClienteDto.class));
+        var pageResponse = new PageResponse<ClienteDto>(
+                clientesDto.getContent(),
+                clientesDto.getTotalPages(),
+                clientesDto.getTotalElements(),
+                clientesDto.getNumber() + 1);
+
+        return pageResponse;
     }
 }
