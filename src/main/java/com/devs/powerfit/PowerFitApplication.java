20d653a9c4c5fd76539bfe48467f7b4c9a326674
Marcelo Ferreira <marceloferreira@fiuni.edu.py>
2024-02-21 23:18:42 -0300

    Creacion del proyecto-  agregado de dependencias

---
+++ src/main/java/com/devs/powerfit/PowerFitApplication.java
@@ -0,0 +1,13 @@
+package com.devs.powerfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerFitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PowerFitApplication.class, args);
+    }
+
+}
