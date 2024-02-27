5be9041d35390ae733cd51039d7047ec0ee321e0
Marcelo Ferreira <marceloferreira@fiuni.edu.py>
2024-02-27 11:47:09 -0300

    Terminado el servicio y controller de suscripcionCliente

--- src/main/java/com/devs/powerfit/controllers/suscripciones/SuscripcionController.java
+++ src/main/java/com/devs/powerfit/controllers/suscripciones/SuscripcionController.java
@@ -1,9 +1,53 @@
 package com.devs.powerfit.controllers.suscripciones;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.devs.powerfit.dtos.suscripciones.SuscripcionDto;
+import com.devs.powerfit.utils.responses.PageResponse;
+import com.devs.powerfit.interfaces.suscripciones.ISuscripcionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/suscripciones")
 public class SuscripcionController {
+    private final ISuscripcionService suscripcionService;
+
+    @Autowired
+    public SuscripcionController(ISuscripcionService suscripcionService) {
+        this.suscripcionService = suscripcionService;
+    }
+
+    @PostMapping
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<SuscripcionDto> create(@RequestBody SuscripcionDto suscripcionDto) {
+        return new ResponseEntity<>(suscripcionService.create(suscripcionDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SuscripcionDto> getById(@PathVariable Long id) {
+        SuscripcionDto suscripcionDto = suscripcionService.getById(id);
+        return new ResponseEntity<>(suscripcionDto, HttpStatus.OK);
+    }
+
+    @GetMapping("/page/{page}")
+    public ResponseEntity<PageResponse<SuscripcionDto>> getAll(@PathVariable int page) {
+        PageResponse<SuscripcionDto> suscripcionesPage = suscripcionService.getAll(page);
+        return new ResponseEntity<>(suscripcionesPage, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<SuscripcionDto> update(@PathVariable Long id, @RequestBody SuscripcionDto suscripcionDto) {
+        SuscripcionDto updatedSuscripcion = suscripcionService.update(id, suscripcionDto);
+        return new ResponseEntity<>(updatedSuscripcion, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Boolean> delete(@PathVariable Long id) {
+        boolean deleted = suscripcionService.delete(id);
+        return new ResponseEntity<>(deleted, HttpStatus.OK);
+    }
 }
--- src/main/java/com/devs/powerfit/daos/suscripciones/SuscripcionDao.java
+++ src/main/java/com/devs/powerfit/daos/suscripciones/SuscripcionDao.java
@@ -1,10 +1,20 @@
 package com.devs.powerfit.daos.suscripciones;
 
+import com.devs.powerfit.beans.clientes.ClienteBean;
 import com.devs.powerfit.beans.suscripciones.SuscripcionBean;
 import com.devs.powerfit.dtos.suscripciones.SuscripcionDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SuscripcionDao extends JpaRepository<SuscripcionBean, Long> {
+    Optional<SuscripcionBean> findByIdAndActiveTrue(Long id);
+    Optional<SuscripcionBean> findByClienteIdAndActiveTrue(Long id);
+    Page<SuscripcionBean> findAllByActiveTrue(Pageable pageable);
+
+
 }
