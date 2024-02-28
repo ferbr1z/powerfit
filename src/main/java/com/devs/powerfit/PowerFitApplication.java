c2c6c62f9c3d50596e7a7c7be8ddb8ce43559df3
Marcelo Ferreira <marceloferreira@fiuni.edu.py>
2024-02-28 12:29:52 -0300

    Agregue la configuracion y dependencia de redis, tambien agregue las anotaciones correspondientes de cache en los servicios del api

--- src/main/java/com/devs/powerfit/PowerFitApplication.java
+++ src/main/java/com/devs/powerfit/PowerFitApplication.java
@@ -2,8 +2,10 @@ package com.devs.powerfit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class PowerFitApplication {
 
     public static void main(String[] args) {
