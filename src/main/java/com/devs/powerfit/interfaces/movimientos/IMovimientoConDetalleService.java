e0446b7f397273415c7baae0f2fe5f39a92f2bdd
MarceloDanielFerreira <113811868+MarceloDanielFerreira@users.noreply.github.com>
2024-03-26 12:43:35 -0400

    Establecidas la estructura para el servicio de movimientos

---
+++ src/main/java/com/devs/powerfit/interfaces/movimientos/IMovimientoConDetalleService.java
@@ -0,0 +1,7 @@
+package com.devs.powerfit.interfaces.movimientos;
+
+import com.devs.powerfit.dtos.movimientos.MovimientoConDetalleDto;
+import com.devs.powerfit.interfaces.bases.IService;
+
+public interface IMovimientoConDetalleService extends IService<MovimientoConDetalleDto> {
+}
