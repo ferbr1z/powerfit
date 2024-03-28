ccc6e5442e4e3ec1461340f26a1c47936f7cb0a8
MarceloDanielFerreira <113811868+MarceloDanielFerreira@users.noreply.github.com>
2024-03-28 16:58:14 -0400

    Buscar caja por nombre

--- src/main/java/com/devs/powerfit/interfaces/cajas/ICajaService.java
+++ src/main/java/com/devs/powerfit/interfaces/cajas/ICajaService.java
@@ -2,6 +2,8 @@ package com.devs.powerfit.interfaces.cajas;
 
 import com.devs.powerfit.dtos.cajas.CajaDto;
 import com.devs.powerfit.interfaces.bases.IService;
+import com.devs.powerfit.utils.responses.PageResponse;
 
 public interface ICajaService extends IService<CajaDto> {
+    PageResponse<CajaDto> searchByNombre(String nombre, int page);
 }
