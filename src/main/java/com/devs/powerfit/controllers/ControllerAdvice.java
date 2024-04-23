859b36688c5ba7982ebb7f049a20743d5cd82ff2
Fernando Brizuela <ffba97@gmail.com>
2024-04-23 19:26:13 -0400

    Fix: Ahora tira error 400 cuando hay un error en la petición

--- src/main/java/com/devs/powerfit/controllers/ControllerAdvice.java
+++ src/main/java/com/devs/powerfit/controllers/ControllerAdvice.java
@@ -5,6 +5,8 @@ import com.devs.powerfit.exceptions.BaseException;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,6 +14,17 @@ import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ControllerAdvice {
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorDto> handleValidationExceptions(HttpMessageNotReadableException e) {
+        String message = e.getMessage();
+
+        ErrorDto errorDto = ErrorDto.builder()
+                .code(HttpStatus.BAD_REQUEST.toString())
+                .message(message)
+                .build();
+
+        return new ResponseEntity<>(errorDto, HttpStatus.BAD_REQUEST);
+    }
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ErrorDto> runtimeExeptionHandler(RuntimeException e) {
         ErrorDto errorDto = ErrorDto.builder()
