ac02e90565227d37ed817d863ec664c8c5fe1194
arvidjj <andy.oj14@gmail.com>
2024-04-25 14:46:01 -0400

    Mime messages with html

--- src/main/java/com/devs/powerfit/services/email/EmailService.java
+++ src/main/java/com/devs/powerfit/services/email/EmailService.java
@@ -5,9 +5,12 @@ import com.devs.powerfit.beans.suscripciones.SuscripcionBean;
 import com.devs.powerfit.daos.clientes.ClienteDao;
 import com.devs.powerfit.daos.suscripciones.SuscripcionDao;
 import com.devs.powerfit.enums.EEstado;
+import com.devs.powerfit.utils.Setting;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -15,6 +18,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
+
+import java.io.File;
 
 @Service(value= "MailService")
 @Transactional
@@ -56,12 +61,15 @@ public class EmailService {
 
     public void sendEmailWithHtmlTemplate(String to, String subject, String templateName, Context context) {
         MimeMessage mimeMessage = emailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "UTF-8");
         try {
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
             helper.setTo(to);
             helper.setSubject(subject);
             String htmlContent = templateEngine.process(templateName, context);
             helper.setText(htmlContent, true);
+
+            FileSystemResource res = new FileSystemResource(new File("src/main/resources/images/pwflogo1.png"));
+            helper.addInline("pwflogo1", res);
             emailSender.send(mimeMessage);
         } catch (MessagingException e) {
             e.printStackTrace();
@@ -80,9 +88,14 @@ public class EmailService {
         var morosos = clienteDao.findClientsWithPendingSubscriptions();
         for (ClienteBean moroso : morosos) {
             //get pageable of this Page<SuscripcionBean>findAllByClienteIdAndEstadoAndActiveTrue(Pageable pageable,Long id,  EEstado estado);
-            var suscripciones = suscripcionDao.findAllByClienteIdAndEstadoAndActiveTrue(moroso.getId(), EEstado.PENDIENTE);
+            var pag = PageRequest.of(0, Setting.PAGE_SIZE);
+            var suscripcionDetalles = suscripcionDao.findAllByClienteIdAndEstadoAndActiveTrue(pag, moroso.getId(), EEstado.PENDIENTE);
+
             Context context = new Context();
-            context.setVariable("message", moroso.getNombre());
+            String year = String.valueOf(java.time.LocalDate.now().getYear());
+            context.setVariable("year", year);
+            context.setVariable("subscriptions", suscripcionDetalles.getContent());
+            context.setVariable("nombre", moroso.getNombre());
             String subject = "Powerfit: Recordatorio de pago";
 
             sendEmailWithHtmlTemplate(moroso.getEmail(), subject, "email-template", context);
