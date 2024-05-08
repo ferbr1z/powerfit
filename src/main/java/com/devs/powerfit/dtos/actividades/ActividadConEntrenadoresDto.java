5742f7a287fdcb06fa79e5662ef85d5a307e790c
arvidjj <andy.oj14@gmail.com>
2024-05-07 22:50:16 -0400

    Nueva ruta para actividades con entrenadores

--- src/main/java/com/devs/powerfit/dtos/actividades/ActividadConEntrenadoresDto.java
+++ src/main/java/com/devs/powerfit/dtos/actividades/ActividadConEntrenadoresDto.java
@@ -2,12 +2,16 @@ package com.devs.powerfit.dtos.actividades;
 
 import com.devs.powerfit.abstracts.AbstractDto;
 import com.devs.powerfit.dtos.empleados.EmpleadoDto;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
-public class ActividadConEntrenadoresDto extends AbstractDto {
+@AllArgsConstructor
+@NoArgsConstructor
+public class ActividadConEntrenadoresDto {
     private ActividadDto actividad;
     List<EmpleadoDto> entrenadores;
 }
