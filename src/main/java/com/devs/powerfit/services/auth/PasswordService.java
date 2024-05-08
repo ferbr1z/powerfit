56ad325d5d772a8b31ea37ebef863ac08870e3e1
Fernando Brizuela <ffba97@gmail.com>
2024-05-08 18:04:58 -0400

    fix: activar token de recuperación por defecto

--- src/main/java/com/devs/powerfit/services/auth/PasswordService.java
+++ src/main/java/com/devs/powerfit/services/auth/PasswordService.java
@@ -71,6 +71,8 @@ public class PasswordService {
                 .usado(false)
                 .build();
 
+        recoveryToken.setActive(true);
+
         _recoveryTokeService.save(recoveryToken);
 
         _mail.sendRecoveryPasswordEmail(user, token);
