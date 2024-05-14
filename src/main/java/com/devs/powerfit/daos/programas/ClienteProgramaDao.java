b753bc75fbccecfc3b0bde03cc6aaa01b52278a6
Fernando Brizuela <ffba97@gmail.com>
2024-05-14 03:06:09 -0400

    Corregido errores + endpoints actualizados

--- src/main/java/com/devs/powerfit/daos/programas/ClienteProgramaDao.java
+++ src/main/java/com/devs/powerfit/daos/programas/ClienteProgramaDao.java
@@ -9,19 +9,31 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 @Repository
 public interface ClienteProgramaDao extends JpaRepository<ClienteProgramaBean, Long> {
 
-    @Query("SELECT cp FROM ClienteProgramaBean cp WHERE cp.programa.id = :programaId AND cp.id = :id AND cp.active = true")
+    @Query("SELECT cp FROM ClienteProgramaBean cp " +
+            "WHERE cp.programa.id = :programaId " +
+            "AND cp.id = :id " +
+            "AND cp.active = true")
     Optional<ClienteProgramaBean> findByProgramaIdAndId(@Param("programaId") Long programaId, @Param("id") Long id);
-
 
     @Query("SELECT NEW com.devs.powerfit.dtos.programas.clientePrograma.ClienteProgramaDto(cp.id, cp.active, p.id, p.titulo, c.id, c.nombre, cp.fechaEvaluacion) " +
             "FROM ClienteProgramaBean cp " +
             "JOIN cp.cliente c " +
             "JOIN cp.programa p " +
-            "WHERE p.id = :programaId AND cp.active = true")
-    Page<ClienteProgramaDto> findAllByProgramaId(@Param("programaId") Long programaId, Pageable page);
+            "WHERE p.id = :programaId " +
+            "AND cp.active = true " +
+            "AND c.active = true " +
+            "AND (:nombreCliente IS NULL or LOWER(cp.cliente.nombre) LIKE CONCAT('%', LOWER(:nombreCliente), '%')) " +
+            "AND (:fechaInicio IS NULL OR :fechaFin IS NULL OR cp.fechaEvaluacion BETWEEN :fechaInicio AND :fechaFin)")
+    Page<ClienteProgramaDto> findAllByProgramaId(
+            @Param("programaId") Long programaId,
+            @Param("nombreCliente") String nombreCliente,
+            @Param("fechaInicio") LocalDate fechaInicio,
+            @Param("fechaFin") LocalDate fechaFin,
+            Pageable page);
 }
--- src/main/java/com/devs/powerfit/utils/mappers/programaMapper/ClienteProgramaMapper.java
+++ src/main/java/com/devs/powerfit/utils/mappers/programaMapper/ClienteProgramaMapper.java
@@ -2,6 +2,7 @@ package com.devs.powerfit.utils.mappers.programaMapper;
 
 import com.devs.powerfit.abstracts.AbstractMapper;
 import com.devs.powerfit.beans.programas.ClienteProgramaBean;
+import com.devs.powerfit.dtos.programas.clientePrograma.BaseClienteProgramDto;
 import com.devs.powerfit.dtos.programas.clientePrograma.ClienteProgramaDto;
 import org.modelmapper.PropertyMap;
 import org.springframework.stereotype.Component;
@@ -34,6 +35,15 @@ public class ClienteProgramaMapper extends AbstractMapper<ClienteProgramaBean, C
             }
         });
 
+        modelMapper.addMappings(new PropertyMap<BaseClienteProgramDto, ClienteProgramaBean>() {
+
+            @Override
+            protected void configure() {
+                map().getPrograma().setId(source.getProgramaId());
+                map().getCliente().setId(source.getClienteId());
+            }
+        });
+
     }
 
     @Override
@@ -44,5 +54,12 @@ public class ClienteProgramaMapper extends AbstractMapper<ClienteProgramaBean, C
     @Override
     public ClienteProgramaDto toDto(ClienteProgramaBean bean) {
         return modelMapper.map(bean, ClienteProgramaDto.class);
+    }
+
+    public ClienteProgramaBean toBean(BaseClienteProgramDto dto) {
+        return modelMapper.map(dto, ClienteProgramaBean.class);
+    }
+    public BaseClienteProgramDto toBaseDto(ClienteProgramaBean bean) {
+        return modelMapper.map(bean, BaseClienteProgramDto.class);
     }
 }
