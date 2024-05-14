8cb9b4b58d33411e1a294d72fb5bc00ca7d26c6e
arvidjj <andy.oj14@gmail.com>
2024-05-14 19:42:41 -0400

    implementado endpoint con query

--- src/main/java/com/devs/powerfit/interfaces/programas/IProgramaService.java
+++ src/main/java/com/devs/powerfit/interfaces/programas/IProgramaService.java
@@ -1,5 +1,6 @@
 package com.devs.powerfit.interfaces.programas;
 
+import com.devs.powerfit.dtos.programas.CantClientesProgramaDto;
 import com.devs.powerfit.dtos.programas.ProgramaDto;
 import com.devs.powerfit.dtos.programas.ProgramaForListDto;
 import com.devs.powerfit.dtos.programas.ProgramaItemDto;
@@ -8,9 +9,11 @@ import com.devs.powerfit.enums.ENivelPrograma;
 import com.devs.powerfit.enums.ESexo;
 import com.devs.powerfit.interfaces.bases.IService;
 import com.devs.powerfit.utils.responses.PageResponse;
+import org.springframework.data.domain.Page;
 
 public interface IProgramaService extends IService<ProgramaDto> {
     public PageResponse<ProgramaForListDto> getAll(int page, String titulo, ENivelPrograma nivel, ESexo sexo);
 
 
+    public PageResponse<CantClientesProgramaDto> getCantClientesByEntrenadorPrograma(Long entrenadorId, int page);
 }
